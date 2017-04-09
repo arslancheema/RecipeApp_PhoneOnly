841566236bdf99d36e9832cbc304e70c539dd391
Arslan Arshad <aac.1989@hotmail.com>
2017-04-09 17:44:17 +0900

    Phone Only Fragements App

---
+++ app/src/main/java/com/teamtreehouse/smellslikebakin/ViewPagerFragment.java
@@ -0,0 +1,64 @@
+package com.teamtreehouse.smellslikebakin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ViewPagerFragment extends Fragment {
+    public static final String KEY_RECIPE_INDEX = "recipe_index";
+    public static final String KEY_IS_INGREDIENTS = "key_is_ingredients";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
+        getActivity().setTitle(Recipes.names[index]);
+        View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final CheckBoxesFragment ingredientsFragment = new CheckBoxesFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX, index);
+        bundle.putBoolean(KEY_IS_INGREDIENTS, true);
+        ingredientsFragment.setArguments(bundle);
+        final CheckBoxesFragment directionsFragment = new CheckBoxesFragment();
+        bundle = new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX, index);
+        bundle.putBoolean(KEY_IS_INGREDIENTS, false);
+        directionsFragment.setArguments(bundle);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position == 0 ? "Ingredients" : "Directions";
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
+    }
+}
